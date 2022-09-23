6c6331a7f1439aedbc7a44a7928072b57f195c62
ZafarUstad <zafarustad123@gmail.com>
2022-09-23 18:58:47 +0530

    utils

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {Provider} from 'react-redux';
 import {store} from './src/store/store';
-import Router from './src/Router/router';
+import Router from './src/router/Router';
 
 const App = () => {
   return (
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import Config from 'react-native-config';
 
 const Home = () => {
+  console.log(Config.API_URL);
   return (
     <View>
       <Text style={{color: '#000'}}>Home</Text>
